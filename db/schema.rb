ac8dbf0e8cc8306503bdf15151940bfaf75deaf7
EC2 Default User <ec2-user@ip-172-31-93-86.ec2.internal>
2021-08-11 18:14:57 +0000

    [Update]bolgs/newで投稿後にindexに戻れないエラーを解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_083259) do
+ActiveRecord::Schema.define(version: 2021_08_11_181148) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
-    t.string "categoly"
+    t.string "category"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
